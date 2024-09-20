f185e5ee93bd5790c094896a369ece96c56c60f9
Manprak <vmmanzanop@ufpso.edu.co>
2024-09-20 17:44:33 -0500

    Refactor complete for quiz

--- src/AgeValidation.java
+++ src/AgeValidation.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
 
 public class AgeValidation {
-    // Definir la constante para el límite de edad
-    private static final int AGE_LIMIT = 18;
+    final int AGE_LIMIT = 18;
 
     public static void main(String[] args) {
-        // Crear un objeto Scanner para la entrada del usuario
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar al usuario que ingrese su edad
         System.out.print("Ingrese su edad: ");
         int age = scanner.nextInt();
 
-        // Validar la edad y mostrar el mensaje correspondiente
         if (age >= AGE_LIMIT) {
             System.out.println("Acceso aceptado");
         } else {
             System.out.println("Acceso denegado");
         }
-
-        // Cerrar el scanner
         scanner.close();
     }
 }
--- src/Store.java
+++ src/Store.java
@@ -1,22 +1,19 @@
 public class Store {
-    // Definir constantes
-    private static final int[] PRECIOS_PRODUCTOS = {20, 5, 10}; // Precios de los productos
-    private static final int[] CANTIDADES_PRODUCTOS = {1, 5, 8}; ; // Cantidades vendidas
-    private static final int UMbral_VENTAS = 50; // Umbral para el desempeño en ventas
+     final int[] PRECIOS_PRODUCTOS = {20, 5, 10}; 
+     final int[] CANTIDADES_PRODUCTOS = {1, 5, 8}; ; 
+     final int UMbral_VENTAS = 50; 
     
     public static void main(String[] args) {
-        // Validar que los arreglos de precios y cantidades tengan el mismo tamaño
-        if (PRECIOS_PRODUCTOS.length != CANTIDADES_PRODUCTOS.length) {
+        if (PRECIOS_PRODUCTOS != CANTIDADES_PRODUCTOS) {
             System.out.println("Error: Los arreglos de precios y cantidades no coinciden.");
-            return;
+            
         }
-        // Calcular las ventas totales para cada producto
         int totalVentas = 0;
         for (int i = 0; i < PRECIOS_PRODUCTOS.length; i++) {
             int totalProducto = PRECIOS_PRODUCTOS[i] * CANTIDADES_PRODUCTOS[i];
             totalVentas += totalProducto;
         }
-        // Determinar y mostrar el desempeño de ventas
+       
         if (totalVentas > UMbral_VENTAS) {
             System.out.println("Buen desempeño en ventas");
         } else {
